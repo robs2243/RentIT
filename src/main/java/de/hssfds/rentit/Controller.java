d5a35d97e61c64b33b7a92d39e7bf4d45d4a57dc
Robert Reinhardt <robs2243@gmail.com>
2026-02-06 12:53:45 +0100

    rentit.fxml added

--- src/main/java/de/hssfds/rentit/Controller.java
+++ src/main/java/de/hssfds/rentit/Controller.java
@@ -3,7 +3,7 @@ package de.hssfds.rentit;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
