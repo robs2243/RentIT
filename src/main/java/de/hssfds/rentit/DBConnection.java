ba65c713a9760ded102ef636b0eb47c865d36df7
Robert Reinhardt <robs2243@gmail.com>
2026-02-06 14:46:53 +0100

    DBConnection KLasse hingefügt

---
+++ src/main/java/de/hssfds/rentit/DBConnection.java
@@ -0,0 +1,54 @@
+package de.hssfds.rentit;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/vermietung?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "2243robs";
+
+    private static Connection connection = null;
+
+    private DBConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        try {
+
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+                System.out.println("Verbindet!");
+            }
+        } catch (SQLException e) {
+            System.err.println("   URL: " + URL);
+            System.err.println("   User: " + USER);
+            System.err.println("   Fehler: " + e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+        return connection;
+    }
+
+    /**
+     * Закрыть подключение к базе данных
+     */
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+                System.out.println("Verbindung geschlossen.");
+            } catch (SQLException e) {
+                System.err.println("Fehler!");
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Es gab kein Verbindung.");
+        }
+    }
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module de.hssfds.rentit {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens de.hssfds.rentit to javafx.fxml;
