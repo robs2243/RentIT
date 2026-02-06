3391699823f600e6ebbf6ed648afe4c4df77f98f
Robert Reinhardt <robs2243@gmail.com>
2026-02-06 13:08:49 +0100

    renamed fxml

--- src/main/java/de/hssfds/rentit/Controller.java
+++ src/main/java/de/hssfds/rentit/Controller.java
@@ -11,4 +11,94 @@ public class Controller {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private void onVermieterAdd() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onVermieterUpdate() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onVermieterDelete() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onVermieterClear() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onMieterAdd() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onMieterUpdate() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onMieterDelete() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onMieterClear() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onObjektAdd() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onObjektUpdate() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onObjektDelete() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onObjektClear() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onVermietungAdd() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onVermietungUpdate() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onVermietungDelete() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onVermietungClear() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onCheckAvailability() {
+        // TODO: Implementierung ergänzen
+    }
+
+    @FXML
+    private void onShowObjektInfo() {
+        // TODO: Implementierung ergänzen
+    }
 }
--- src/main/java/de/hssfds/rentit/Main.java
+++ src/main/java/de/hssfds/rentit/Main.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 public class Main extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("RentIT.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
