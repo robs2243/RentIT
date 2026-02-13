a6b690ccc0bb47a09330f5680e1ad457ccf14bea
Robert Reinhardt <robs2243@gmail.com>
2026-02-13 08:54:46 +0100

    useSSL=true gesetzt wegen dem public key retrieval problem

--- src/main/java/de/hssfds/rentit/DBConnection.java
+++ src/main/java/de/hssfds/rentit/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/vermietung?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/vermietung?useSSL=true&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "2243robs";
 
